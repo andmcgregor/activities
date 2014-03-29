70d0dbdfa438e7cf4c1c25448b9d966f095e80aa
Andrew McGregor <me@andrewmcgregor.me>
2014-03-29 04:04:07 -0400

    adds initial timeline logic

--- public/core.js
+++ public/core.js
@@ -1,11 +1,35 @@
 var activities = angular.module('activities', []);
 
 function main($scope, $http) {
-  $scope.days = [
-    {x: 0, y: 0},
-    {x: 10, y: 13},
-    {x: 30, y: 26}
-  ];
+  day_size = 20;
+  space_size = 2;
+
+  date = new Date();
+  date.setFullYear(date.getFullYear() - 1);
+  current_day = date.getDay();
+
+  current_x = space_size;
+  current_y = space_size * (current_day + 1) + current_day * day_size;
+
+  days = Array(365);
+
+  for(x = 0; x < days.length; x++) {
+    if (current_day > 6) {
+      current_x = current_x + space_size + day_size;
+      current_y = space_size;
+      current_day = 0;
+    }
+
+    days[x] = {
+      x: current_x,
+      y: current_y,
+    };
+
+    current_y = current_y + space_size + day_size;
+    current_day++;
+  }
+
+  $scope.days = days;
 
   $http.get('/api/activities')
     .success(function(data) {
