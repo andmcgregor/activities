ad1ad78168ae4d862e400b8cdd62a56a26f6c6a4
Andrew McGregor <me@andrewmcgregor.me>
2014-03-29 17:54:34 -0400

    integrates github api

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var config = require('./config.json');
 var express  = require('express');
 var app      = express();
 var mongoose = require('mongoose');
+var https    = require('https');
 
 mongoose.connect(config.db);
 
@@ -32,6 +33,71 @@ app.get('/api/activities', function(req, res) {
   });
 });
 
+app.get('/api/update', function() {
+  githubRequest('/user/repos');
+});
+
 app.get('*', function(req, res) {
   res.sendfile('./public/index.html');
 });
+
+// Github API
+
+githubRequest = function(path, other) {
+  var options = {
+    headers: {
+      'User-Agent': config.github_username
+    },
+    auth: config.github_token + ':x-oauth-basic',
+    hostname: 'api.github.com',
+    path: path,
+    method: 'GET'
+  }
+  var str = '';
+
+  var request = https.request(options, function(res) {
+    res.on('data', function(chunk) {
+      str += chunk;
+    });
+
+    res.on('end', function() {
+      data = JSON.parse(str);
+      if (path == '/user/repos') {
+        repo_names = fetchRepos(data);
+        console.log('Successfully Fetched Repos!');
+        // loop through repo names
+        githubRequest('/repos/'+repo_names[1]+'/commits?author='+config.github_username, { repo: repo_names[1] });
+      } else {
+        commits = fetchCommits(data, other.repo);
+        console.log(commits);
+      }
+    });
+  });
+
+  request.end();
+
+  request.on('error', function(e) {
+    console.error(e);
+  });
+}
+
+fetchRepos = function(data) {
+  repo_names = [];
+  for(x = 0; x < data.length; x++) {
+    repo_names.push(data[x].full_name);
+  }
+  return repo_names;
+}
+
+fetchCommits = function(data, repo) {
+  commits = [];
+  for(x = 0; x < data.length; x++) {
+    commits.push({
+      type: "commit",
+      date: data[x].commit.committer.date,
+      project: repo,
+      link: data[x].url
+    });
+  }
+  return commits;
+}
