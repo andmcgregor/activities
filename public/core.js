312562524e64d70614631a4830a5b63156c001f4
Andrew McGregor <me@andrewmcgregor.me>
2014-03-29 02:12:00 -0400

    adds svg element + hardcoded day data

--- public/core.js
+++ public/core.js
@@ -1,6 +1,12 @@
 var activities = angular.module('activities', []);
 
 function main($scope, $http) {
+  $scope.days = [
+    {x: y: 0},
+    {y: 13},
+    {y: 26}
+  ];
+
   $http.get('/api/activities')
     .success(function(data) {
       $scope.activities = data;
