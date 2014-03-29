0a0b695c03ed1e98c20a43675fe76802c2eedc40
Andrew McGregor <me@andrewmcgregor.me>
2014-03-29 01:33:41 -0400

    adds activities module

---
+++ public/core.js
@@ -0,0 +1,10 @@
+var activities = angular.module('activities', []);
+
+function main($scope, $http) {
+  $http.get('/api/activities')
+    .success(function(data) {
+      $scope.activities = data;
+    })
+    .error(function(data) {
+      console.log('Error: ' + data);
+}
