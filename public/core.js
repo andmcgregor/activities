6cf1423d1999e213a1e2063f2b37a57fb338b059
Andrew McGregor <me@andrewmcgregor.me>
2014-04-01 23:16:26 -0400

    implements basic hover stats

--- public/core.js
+++ public/core.js
@@ -1,109 +1,127 @@
 var activities = angular.module('activities', []);
 
-function main($scope, $http) {
-  $http.get('/api/activities').success(function(data) {
-    $scope.content = data.content;
-    activities = data.activities;
+activities.controller('main', ['$scope', '$http',
+  function($scope, $http) {
+    $http.get('/api/activities').success(function(data) {
+      // move logic to server side
+      $scope.content = data.content;
+      activities = data.activities;
 
-    day_size = 20;
-    space_size = 2;
+      day_size = 20;
+      space_size = 2;
 
-    date = new Date();
-    date.setFullYear(date.getFullYear() - 1);
-    date = new Date(date.getTime() + 86400000);
-    current_day = date.getDay();
+      date = new Date();
+      date.setFullYear(date.getFullYear() - 1);
+      date = new Date(date.getTime() + 86400000);
+      current_day = date.getDay();
 
-    current_x = space_size;
-    current_y = space_size * (current_day + 1) + current_day * day_size;
+      current_x = space_size;
+      current_y = space_size * (current_day + 1) + current_day * day_size;
 
-    date.setHours(0,0,0,0);
-    date_in_ms = date.getTime() / 1000;
+      date.setHours(0,0,0,0);
+      date_in_ms = date.getTime() / 1000;
 
-    commit_num = 0;
+      commit_num = 0;
 
-    days = Array(365);
+      days = Array(365);
 
-    for(x = 0; x < days.length; x++) {
-      if (current_day > 6) {
-        current_x = current_x + space_size + day_size;
-        current_y = space_size;
-        current_day = 0;
+      for(x = 0; x < days.length; x++) {
+        if (current_day > 6) {
+          current_x = current_x + space_size + day_size;
+          current_y = space_size;
+          current_day = 0;
+        }
+
+        var secret = false;
+        for(y = 0; y < activities.length; y++) {
+          if(activities[y].date > date_in_ms && activities[y].date < date_in_ms + 86400) {
+            commit_num++;
+            if(activities[y].secret == true) {
+              secret = true;
+            }
+          }
+        }
+
+        days[x] = {
+          x: current_x,
+          y: current_y,
+          start: date_in_ms,
+          end: date_in_ms + 86399,
+          date: date.toString(),
+          commit_num: commit_num,
+          secret: secret
+        };
+
+        date = new Date(date.getTime() + 86400000);
+
+        current_y = current_y + space_size + day_size;
+        date_in_ms = date_in_ms + 86400;
+        current_day++;
+        commit_num = 0;
       }
 
-      var secret = false;
-      for(y = 0; y < activities.length; y++) {
-        if(activities[y].date > date_in_ms && activities[y].date < date_in_ms + 86400) {
-          commit_num++;
-          if(activities[y].secret == true) {
-            secret = true;
-          }
+      // calculate color values
+      var none  = '#EEEEEE';
+      var some  = '#89C4F4';
+      var more  = '#4B77BE';
+      var lots  = '#34495E';
+      var heaps = '#2C3E50';
+
+      max = Math.max.apply(Math, days.map(function(day) { return day.commit_num }));
+      for(x = 0; x < days.length; x++) {
+        foo = days[x].commit_num / max;
+        switch (true) {
+          case foo == 0:
+            days[x].color = none;
+            break;
+          case foo > 0 && foo < 0.25:
+            days[x].color = some;
+            break;
+          case foo >= 0.25 && foo < 0.5:
+            days[x].color = more;
+            break;
+          case foo >= 0.5 && foo < 0.75:
+            days[x].color = lots;
+            break;
+          case foo > 0.75:
+            days[x].color = heaps;
+            break;
         }
       }
 
-      days[x] = {
-        x: current_x,
-        y: current_y,
-        start: date_in_ms,
-        end: date_in_ms + 86399,
-        commit_num: commit_num,
-        secret: secret
-      };
+      $scope.days = days;
+      $scope.activities = activities;
 
-      current_y = current_y + space_size + day_size;
-      date_in_ms = date_in_ms + 86400;
-      current_day++;
-      commit_num = 0;
-    }
-
-    // calculate color values
-    var none  = '#EEEEEE';
-    var some  = '#89C4F4';
-    var more  = '#4B77BE';
-    var lots  = '#34495E';
-    var heaps = '#2C3E50';
-
-    max = Math.max.apply(Math, days.map(function(day) { return day.commit_num }));
-    for(x = 0; x < days.length; x++) {
-      foo = days[x].commit_num / max;
-      switch (true) {
-        case foo == 0:
-          days[x].color = none;
-          break;
-        case foo > 0 && foo < 0.25:
-          days[x].color = some;
-          break;
-        case foo >= 0.25 && foo < 0.5:
-          days[x].color = more;
-          break;
-        case foo >= 0.5 && foo < 0.75:
-          days[x].color = lots;
-          break;
-        case foo > 0.75:
-          days[x].color = heaps;
-          break;
+      totals = {
+        commits: 0,
+        pulls:   0,
+        repos:   0
       }
-    }
-
-    $scope.days = days;
-    $scope.activities = activities;
-
-    totals = {
-      commits: 0,
-      pulls:   0,
-      repos:   0
-    }
-    repos = {}
-    for(x = 0; x < activities.length; x++) {
-      if(!repos[activities[x].owner+'/'+activities[x].repo]) {
-        repos[activities[x].owner+'/'+activities[x].repo] = 1;
-        totals.repos++;
-      }
-      if(activities[x].type == "commit") {
+      repos = {}
+      for(x = 0; x < activities.length; x++) {
+        if(!repos[activities[x].owner+'/'+activities[x].repo]) {
+          repos[activities[x].owner+'/'+activities[x].repo] = 1;
+          totals.repos++;
+        }
+        if(activities[x].type == "commit") {
         totals.commits++;
-      } else {
-        totals.pulls++;
+        } else {
+          totals.pulls++;
+        }
       }
+      $scope.totals = totals;
+    });
+
+    $scope.dayHover = function(day, event) {
+      $('.hover').html(day.commit_num+' contributions on '+day.date);
+      $('.hover').show();
+      offset = $(event.target).offset();
+      offset.top -= 20;
+      offset.left -= 90;
+      $('.hover').offset(offset);
     }
-    $scope.totals = totals;
-  });
-}
+
+    $scope.removeHover = function() {
+      $('.hover').hide();
+    }
+}]);
