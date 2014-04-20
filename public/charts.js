398ebdb548fde25c012596a5d30fd2d27afcfa72
Andrew McGregor <me@andrewmcgregor.me>
2014-04-20 19:12:26 -0400

    fixes labels but no transitions yet

--- public/charts.js
+++ public/charts.js
@@ -18,7 +18,7 @@ function Chart(data, size, colors) {
                                      .attr('transform', 'translate('+(size/2)+','+(size/2)+')');
 
   this.draw();
-  //this.label(data);
+  this.label();
 }
 
 Chart.prototype.update = function (newData) {
@@ -30,10 +30,10 @@ Chart.prototype.update = function (newData) {
     }
   }
 
-  this.path = this.path.data(this.pie(this.data));
+  this.arcs = this.arcs.data(this.pie(this.data));
 
   var arc = this.arc;
-  this.path.transition().duration(750).attrTween('d', function(a) {
+  this.arcs.transition().duration(750).attrTween('d', function(a) {
     var i = d3.interpolate(this._current, a);
     this._current = i(0);
     return function(t) {
@@ -44,24 +44,25 @@ Chart.prototype.update = function (newData) {
 
 Chart.prototype.draw = function() {
   var t = this;
-  this.path = this.svg.datum(this.data).selectAll('path').data(this.pie)
-                                               .enter().append('path')
-                                               .attr('fill', function(d, i) { return t.color(i); })
-                                               .attr('d', this.arc)
-                                               .each(function(d) { this._current = d; });
-
-}
+  this.arcs = this.svg.datum(this.data).selectAll('path').data(this.pie)
+                                       .enter().append('path')
+                                       .attr('fill', function(d, i) { return t.color(i); })
+                                       .attr('d', this.arc)
+                                       .each(function(d) { this._current = d; });
+  }
 
 Chart.prototype.label = function(data) {
-  var t = this;
-  this.arcs.append('svg:text').attr('transform', function(d) {
-    var c = t.arc.centroid(d),
-        x = c[0],
-        y = c[1],
-        h = Math.sqrt(x * x + y * y),
-        r = 150;
-    return 'translate('+ (x/h * r) + ',' + (y/h * r) + ')';
-  })
-  .attr('text-anchor', 'middle')
-  .text(function(d, i) { return data[i].name; });
+  var arc = this.arc;
+  var data = this.data;
+this.labels = this.svg.datum(this.data).selectAll('text').data(this.pie)
+                    .enter().append('text').attr('transform', function(d) {
+                      var c = arc.centroid(d),
+                          x = c[0],
+                          y = c[1],
+                          h = Math.sqrt(x * x + y * y),
+                          r = 150;
+                      return 'translate('+ (x/h * r) + ',' + (y/h * r) + ')';
+                    })
+                    .attr('text-anchor', 'middle')
+                    .text(function(d, i) { return data[i].name });
 }
