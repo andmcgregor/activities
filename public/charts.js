32e17181c6d5eb9c643eb35021d349e79d26b2ae
Andrew McGregor <me@andrewmcgregor.me>
2014-04-20 21:20:29 -0400

    segment labels now animating

--- public/charts.js
+++ public/charts.js
@@ -25,12 +25,15 @@ Chart.prototype.update = function (newData) {
   for (x = 0; x < this.data.length; x++) {
     if (newData[this.data[x].name]) {
       this.data[x].count = newData[this.data[x].name];
+      $('[id="'+window.btoa(this.data[x].name)+'"]').show()
     } else {
       this.data[x].count = 0;
+      $('[id="'+window.btoa(this.data[x].name)+'"]').hide()
     }
   }
 
   this.arcs = this.arcs.data(this.pie(this.data));
+  this.labels = this.labels.data(this.pie(this.data));
 
   var arc = this.arc;
   this.arcs.transition().duration(750).attrTween('d', function(a) {
@@ -39,6 +42,15 @@ Chart.prototype.update = function (newData) {
     return function(t) {
       return arc(i(t));
     };
+  });
+
+  this.labels.transition().duration(750).attr('transform', function(d) {
+    var c = arc.centroid(d),
+        x = c[0],
+        y = c[1],
+        h = Math.sqrt(x * x + y * y),
+        r = 150;
+    return 'translate('+ (x/h * r) + ',' + (y/h * r) + ')';
   });
 }
 
@@ -54,7 +66,7 @@ Chart.prototype.draw = function() {
 Chart.prototype.label = function(data) {
   var arc = this.arc;
   var data = this.data;
-this.labels = this.svg.datum(this.data).selectAll('text').data(this.pie)
+  this.labels = this.svg.datum(this.data).selectAll('text').data(this.pie)
                     .enter().append('text').attr('transform', function(d) {
                       var c = arc.centroid(d),
                           x = c[0],
@@ -64,5 +76,9 @@ this.labels = this.svg.datum(this.data).selectAll('text').data(this.pie)
                       return 'translate('+ (x/h * r) + ',' + (y/h * r) + ')';
                     })
                     .attr('text-anchor', 'middle')
-                    .text(function(d, i) { return data[i].name });
+                    .attr('id', function(d, i) {
+                      return window.btoa(data[i].name)
+                    })
+                    .text(function(d, i) { return data[i].name })
+                    .each(function(d) { this._current = d });
 }
