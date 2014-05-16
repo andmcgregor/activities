9ac648fe174a57601ccb1e1ca99b4647487941b7
Andrew McGregor <me@andrewmcgregor.me>
2014-05-16 00:08:11 -0400

    adds heroku port

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ var Request = mongoose.model('Request', {
   repo: String,
 });
 
-app.listen(8000);
+app.listen(process.env.PORT || 8000);
 console.log('Server running at http://127.0.0.1:8000/');
 
 Job.find({}, function(err, jobs) {
